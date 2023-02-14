b838764d6114e8681c117d07efa28ddaba0099a4
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2023-02-14 21:19:01 +0100

    .

--- router_led_web.sh
+++ router_led_web.sh
@@ -21,7 +21,6 @@ LED_6="tp-link:green:wan"
 LED_7="tp-link:green:qss"
 
 # Make sure we have been invoked properly.
-
 if [ "$REQUEST_METHOD" != "GET" ]; then
     echo "<hr>Script Error:"\
     "<br>Usage error, cannot complete request, REQUEST_METHOD!=GET."\
@@ -30,27 +29,39 @@ if [ "$REQUEST_METHOD" != "GET" ]; then
 fi
 
 # If no arguments, exit gracefully now.
-
 if [ -z "$QUERY_STRING" ]; then
     echo "Error: Bro think he query 💀"
     exit 0
-else
-    # No looping this time, just extract the data you are looking for with sed:
-    XX=`echo "$QUERY_STRING" | sed -n 's/^.*led=\([^&]*\).*$/\1/p' | sed "s/%20/ /g"`
-    YY=`echo "$QUERY_STRING" | sed -n 's/^.*state=\([^&]*\).*$/\1/p' | sed "s/%20/ /g"`
-    ZZ=`echo "$QUERY_STRING" | sed -n 's/^.*pass=\([^&]*\).*$/\1/p' | sed "s/%20/ /g"`
-
-    # Check if XX (led number) is in range 0 - 7
-    if [ "$XX" -ge 0 ] && [ "$XX" -le 7 ]; then
-        LED_NAME="$""LED_"$XX
-        eval LED_NAME=$LED_NAME
-        echo $LED_NAME > /root/led.txt
-    else
-        echo "Error: Bro think he LED 💀"
-        exit 0
-    fi
-    
 fi
+
+# Extract the data you are looking for with sed:
+XX=`echo "$QUERY_STRING" | sed -n 's/^.*led=\([^&]*\).*$/\1/p' | sed "s/%20/ /g"`
+YY=`echo "$QUERY_STRING" | sed -n 's/^.*state=\([^&]*\).*$/\1/p' | sed "s/%20/ /g"`
+ZZ=`echo "$QUERY_STRING" | sed -n 's/^.*pass=\([^&]*\).*$/\1/p' | sed "s/%20/ /g"`
+
+# Check if XX (led number) is a real number
+if [ -n "$XX" ] && [ "$XX" -eq "$XX" ] 2>/dev/null; then
+  :
+else
+  echo "Error: Bro think he number 💀"
+   exit 0
+fi
+
+# Check if XX (led number) is in range 0 - 7
+if [ "$XX" -ge 0 ] && [ "$XX" -le 7 ]; then
+    :
+else
+    echo "Error: Bro think he LED 💀"
+    exit 0
+fi
+
+# All good, get led name by id
+LED_NAME="$""LED_"$XX
+eval LED_NAME=$LED_NAME
+
+# Turn on/off the led
+echo $LED_NAME > /root/led.txt
+    
 echo "ඞ ok"
 echo '</body>'
 echo '</html>'
