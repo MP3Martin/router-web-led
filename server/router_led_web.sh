8e31217cde93def456f99ce93905888dd998993a
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2023-02-16 16:04:30 +0100

    Allow any LED number in router_led_web.sh (server)

--- server/router_led_web.sh
+++ server/router_led_web.sh
@@ -4,15 +4,19 @@
 # LICENSE: MIT
 # MADE_BY: https://github.com/MP3Martin
 # 
-# This is a web server to set router LEDs
-# Made for: TL-WR743ND v1 (https://is.gd/3NgWP7)
-# Tested with OpenWrt installed on the router
-# Put this in /www/cgi-bin/
+# This is a web server to set router LEDs.
+# Made for: TL-WR743ND v1 (https://is.gd/3NgWP7), but should work on
+# more routers thanks to the configuration below this introduction.
+#
+# Tested with OpenWrt installed on the router.
+# Put this script in /www/cgi-bin/ and don't forget to "chmod +x" it.
 #
 # Examples:
-# http://ROUTER-IP/cgi-bin/cmd?led=10101010&pass=whentheimposterissus&state=0&mode=1
 # http://ROUTER-IP/cgi-bin/cmd?led=4&pass=whentheimposterissus&state=1
+# http://ROUTER-IP/cgi-bin/cmd?led=10101010&pass=whentheimposterissus&state=0&mode=1
 #
+
+# v v v !!! START OF CONFIGURATION, YOU SHOULD CHANGE THESE VARIABLES !!! v v v
 
 # LED id to name
 LED_0="tp-link\:green\:system"
@@ -23,6 +27,11 @@ LED_4="tp-link\:green\:lan3"
 LED_5="tp-link\:green\:lan4"
 LED_6="tp-link\:green\:wan"
 LED_7="tp-link\:green\:qss"
+
+# Misc globals
+LED_COUNT="8"
+
+# ^ ^ ^ !!! END OF CONFIGURATION, YOU SHOULD CHANGE THESE VARIABLES !!! ^ ^ ^
 
 set_led()
 {
@@ -108,8 +117,9 @@ fi
 #   0 is a single LED (default)
 #   1 is all LEDs at once
 if ! [ "$WW" -eq 1 ]; then
-    # Check if XX (led number) is in range 0 - 7
-    if [ "$XX" -ge 0 ] && [ "$XX" -le 7 ]; then
+    # Check if XX (led number) is in range "0" to "$LED_COUNT - 1"
+    _temp=`expr $LED_COUNT - 1`
+    if [ "$XX" -ge 0 ] && [ "$XX" -le "$_temp" ]; then
         :
     else
         echo "Error: Bro think he LED 💀"
@@ -135,12 +145,12 @@ if ! [ "$WW" -eq 1 ]; then
     # All good, set the LED
     set_led $XX $YY
 else
-    # This mode is way better than sending 8 requests per frame.
+    # This mode is way better than sending "$LED_COUNT" requests per frame.
     # The YY (state) variable will not be needed here.
     
-    # Check if XX (led numberList) is 8 characters big, error if not
-    if ! [ "${#XX}" -eq 8 ]; then
-        echo "Error: Bro think he 8 💀"
+    # Check if XX (led numberList) is "$LED_COUNT" characters big, error if not
+    if ! [ "${#XX}" -eq "$LED_COUNT" ]; then
+        echo "Error: Bro think he $LED_COUNT 💀"
         exit 0
     fi
     
@@ -149,7 +159,7 @@ else
     #   1 = on
     while [ ${#XX} -gt 0 ]; do
         next=${XX#?}
-        index=`expr 8 - ${#XX}`
+        index=`expr $LED_COUNT - ${#XX}`
         set_led $index ${XX%$next}
         XX=$next
     done
